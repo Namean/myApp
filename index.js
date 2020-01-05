9b7fc7f968da1e8a320eab6d3da19d9b78694b90
Namean <dwilsoncnc@gmail.com>
2020-01-05 13:29:47 -0500

    we can load and add data in memory, let's work on persistance

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-data;
-
 function makeWebRequest(method, url) {
 //    method = typeof method !== 'undefined' ? method : 'GET';
     console.log('making web request');
@@ -8,8 +6,34 @@ function makeWebRequest(method, url) {
     xhr.send();
     setTimeout(() => {
         console.log("Data fetch successful!");
-        data = xhr.response;
-    }, 500);
-    
+        data = JSON.parse(xhr.response);
+        console.log(data);
+        main();
+    }, 1500);
 }
 
+
+function Note(title, body) {
+    let d = new Date();
+    this.title = title.trim();
+    this.body = body.trim();
+    this.timestamp = d.getTime();
+}
+
+function addData(arg) {
+    console.log('adding data');
+    data.push(arg);
+}
+
+let btn = document.querySelector('button');
+btn.onclick();
+
+
+function main() {
+   let note = new Note(
+    "Activate phone today",
+    " Head down to T-mobile to get your phone on."
+);
+
+  addData(note); 
+}
